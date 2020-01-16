175dc267b92941573e93c58e0db091a32fc9b0b0
Ohad <53222304+OhadLevine@users.noreply.github.com>
2020-01-16 22:48:17 +0200

    added implementations

--- src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/drivetrain/Drivetrain.java
@@ -9,14 +9,17 @@ package frc.robot.subsystems.drivetrain;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import frc.robot.components.Pigeon;
+import frc.robot.constants.RobotMap;
 import frc.robot.subsystems.MoveableSubsystem;
 
-  /**
-   * Creates a new DrivetrainSubsytem.
-   */
-public class Drivetrain implements MoveableSubsystem  {
+/**
+ * Creates a new DrivetrainSubsytem.
+ */
+public class Drivetrain implements MoveableSubsystem {
 
   WPI_TalonFX leftRearTalon;
   WPI_TalonFX leftMiddleTalon;
@@ -24,75 +27,98 @@ public class Drivetrain implements MoveableSubsystem  {
   WPI_TalonFX rightRearTalon;
   WPI_TalonFX rightMiddleTalon;
   WPI_TalonFX rightFrontTalon;
+  Pigeon gyro;
 
   DifferentialDrive drivetrain;
 
   public Drivetrain() {
-  leftRearTalon = RobotA.
-    
-  }
-
+    leftRearTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_LEFT_REAR_TALON);
+    leftMiddleTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_LEFT_MIDDLE_TALON);
+    leftFrontTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_LEFT_FRONT_TALON);
+    rightRearTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_RIGHT_REAR_TALON);
+    rightMiddleTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_RIGHT_MIDDLE_TALON);
+    rightFrontTalon = new WPI_TalonFX(RobotMap.CAN.DRIVETRAIN_RIGHT_FRONT_TALON);
+    drivetrain = new DifferentialDrive(new SpeedControllerGroup(leftRearTalon, leftMiddleTalon, leftFrontTalon), new SpeedControllerGroup(rightRearTalon, rightMiddleTalon, rightFrontTalon));
+    gyro = new Pigeon(RobotMap.CAN.
   // Drive functions
-  void arcadeDrive(double x, double y){
+  public void arcadeDrive(double x, double y) {
+    drivetrain.arcadeDrive(x, y);
+  }
 
+  public void curvatureDrive(double x, double y, boolean quickTurn) {
+     drivetrain.curvatureDrive(x, y, quickTurn);
   }
-  void curvatureDrive(double x, double y, boolean quickTurn);
-  void tankDrive(double leftSpeed, double rightSpeed);
-  
-  default void voltageTankDrive(double left, double right) {
-      tankDrive(left / RobotController.getBatteryVoltage(), right / RobotController.getBatteryVoltage());
+
+  public void tankDrive(double leftSpeed, double rightSpeed) {
+    drivetrain.tankDrive(leftSpeed, rightSpeed);
   }
-  
+
+  public void voltageTankDrive(double left, double right) {
+    tankDrive(left / RobotController.getBatteryVoltage(), right / RobotController.getBatteryVoltage());
+  }
+
   // Gyro functions
-  double getAngle();
-  
+  double getAngle() {
+    return 0.0;
+  }
+
   default double getRadianAngle() {
-      return Math.toRadians(getAngle());
+    return Math.toRadians(getAngle());
   }
 
   void resetGyro();
+
   void calibrateGyro();
-  
+
   // Encoders functions
   int getRightTicks();
+
   int getLeftTicks();
+
   void resetEncoders();
-  
+
   default double getRightDistance() {
-      return getRightTicks() / Robot.robotConstants.drivetrainConstants.RIGHT_ENCODER_TICKS_PER_METER;
+    return getRightTicks() / Robot.robotConstants.drivetrainConstants.RIGHT_ENCODER_TICKS_PER_METER;
   }
-  
+
   default double getLeftDistance() {
-      return getLeftTicks() / Robot.robotConstants.drivetrainConstants.LEFT_ENCODER_TICKS_PER_METER;
+    return getLeftTicks() / Robot.robotConstants.drivetrainConstants.LEFT_ENCODER_TICKS_PER_METER;
   }
-  
+
   default double getAverageDistance() {
-      return (getLeftDistance() + getRightDistance()) / 2;
+    return (getLeftDistance() + getRightDistance()) / 2;
   }
 
   double getRightVelocity();
+
   double getLeftVelocity();
 
   default double getAverageVelocity() {
-      return (getLeftVelocity() + getRightVelocity()) / 2;
+    return (getLeftVelocity() + getRightVelocity()) / 2;
   }
 
   double getRightAcceleration();
+
   double getLeftAcceleration();
 
   // Motion Profiling functions
   DifferentialDriveKinematics getKinematics();
+
   DifferentialDriveWheelSpeeds getWheelSpeeds();
+
   void resetOdometry(Pose2d pose);
+
   Pose2d getPose();
+
   double getLeftBusVoltage();
+
   double getRightBusVoltage();
+
   double getLeftAppliedOutput();
+
   double getRightAppliedOutput();
 
-  public void move(double power){
-    arcadeDrive.
+  public void move(double power)  
+       arcadeDrive(0, power);
   }
-
-
 }
