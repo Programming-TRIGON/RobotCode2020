b18e6dc4d7bde5db17438c77ddf8ce813fde9b69
Mboesn <moshechelsky@gmail.com>
2020-01-19 21:55:41 +0200

    added maxStall to spin mixer

--- src/main/java/frc/robot/subsystems/mixer/SpinMixer.java
+++ src/main/java/frc/robot/subsystems/mixer/SpinMixer.java
@@ -1,33 +1,47 @@
 package frc.robot.subsystems.mixer;
 
 import java.util.function.DoubleSupplier;
+
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
 /**
- * spin the mixer during the game for putting balls in the ejector.
+ * Spins the mixer during the game for putting balls in the ejector, if the current is too big the motor power flips.
  */
 public class SpinMixer extends CommandBase {
-  DoubleSupplier power;
+  private DoubleSupplier power;
+  private double startTime;
+  private double notOnTarget;
+  private double waitTime;
+  private double maxStall;
 
   /** gets a supplier for motor power */
-  public SpinMixer(DoubleSupplier power) {
+  public SpinMixer(DoubleSupplier power, double maxStall) {
     addRequirements(Robot.mixer);
     this.power = power;
+    this.maxStall = maxStall;
   }
 
   /** gets a double for motor power */
-  public SpinMixer(double power) {
-    this(() -> power);
+  public SpinMixer(double power, double maxStall) {
+    this(() -> power, maxStall);
   }
 
   @Override
   public void initialize() {
+    startTime = Timer.getFPGATimestamp();
   }
 
   @Override
   public void execute() {
-    Robot.mixer.move(power.getAsDouble());
+    if (Robot.mixer.getStall() < maxStall){
+      notOnTarget = Timer.getFPGATimestamp() - startTime;
+    }
+    if (Timer.getFPGATimestamp() - startTime - notOnTarget > waitTime){
+      double speed = -power.getAsDouble();
+      Robot.mixer.move(speed);
+    }
   }
 
   @Override
