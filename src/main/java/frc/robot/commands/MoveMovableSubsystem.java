006c03e98d69dda21d46fa0179eee129f23e2e10
Ohad <ohadlevin99@gmail.com>
2020-01-20 22:51:07 +0200

    fixed according to hilel cr

--- src/main/java/frc/robot/commands/MoveMovableSubsystem.java
+++ src/main/java/frc/robot/commands/MoveMovableSubsystem.java
@@ -1,30 +1,39 @@
 package frc.robot.commands;
 
 import java.util.function.BooleanSupplier;
+import java.util.function.DoubleSupplier;
+
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.MoveableSubsystem;
 
 public class MoveMovableSubsystem extends CommandBase {
   private MoveableSubsystem subsystem;
   private BooleanSupplier isFinished;
-  private double power;
+  private DoubleSupplier power;
 
-  /** This command can move any subsystem that implements MovableSubsytem. */
-  public MoveMovableSubsystem(MoveableSubsystem subsystem, double power, BooleanSupplier isFinished) {
+  /**
+   * This command can move any subsystem that implements MovableSubsytem without
+   * an isFinished
+   */
+  public MoveMovableSubsystem(MoveableSubsystem subsystem, DoubleSupplier power) {
+    this(subsystem, power, () -> false);
+  }
+
+  /**
+   * This command can move any subsystem that implements MovableSubsytem, it uses
+   * isFinished supplier is checked in the isFinished method for checking if the
+   * command is done.
+   */
+  public MoveMovableSubsystem(MoveableSubsystem subsystem, DoubleSupplier power, BooleanSupplier isFinished) {
     this.subsystem = subsystem;
     this.power = power;
     this.isFinished = isFinished;
     addRequirements(subsystem);
   }
-  
-  /** This command can move any subsystem that implements MovableSubsytem without an isFinished */
-  public MoveMovableSubsystem(MoveableSubsystem subsystem, double power) {
-    this(subsystem, power, () -> false);
-  }
 
   @Override
   public void execute() {
-    subsystem.move(power);
+    subsystem.move(power.getAsDouble());
   }
 
   @Override
