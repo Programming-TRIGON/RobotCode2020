56b2589b09aedaa1363755c8fc527925c9e0f1b8
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2020-01-21 22:45:14 +0200

    split SetShooterVelocity into two commands

--- src/main/java/frc/robot/subsystems/shooter/CheesySetShooterVelocity.java
+++ src/main/java/frc/robot/subsystems/shooter/CheesySetShooterVelocity.java
@@ -11,12 +11,16 @@ import static frc.robot.Robot.shooter;
 /**
  * This command spins the wheel in the desired velocity in order to shoot the power cells.
  */
-public class SetShooterVelocity extends CommandBase {
+public class CheesySetShooterVelocity extends CommandBase {
 
 	private static final int LOADED_CELLS_IN_AUTO = 3;
+	private static final int MINIMUM_KF_SAMPLES = 20;
 	private DoubleSupplier velocitySetpoint;
 	private boolean isAuto;
 	private double setpoint;
+	private double kfSamplesAmount;
+	private double leftKfSamplesSum;
+	private double rightKfSamplesSum;
 	private double firstTimeOutsideZone;
 	private double firstTimeInZone;
 	private boolean isInZone;
@@ -27,7 +31,7 @@ public class SetShooterVelocity extends CommandBase {
 	 *
 	 * @see frc.robot.subsystems.shooter.ShooterVelocity#kDefault
 	 */
-	public SetShooterVelocity() {
+	public CheesySetShooterVelocity() {
 		this(false);
 	}
 
@@ -37,14 +41,14 @@ public class SetShooterVelocity extends CommandBase {
 	 * @param isAuto whether the command should stop after shooting 3 cells
 	 * @see frc.robot.subsystems.shooter.ShooterVelocity#kDefault
 	 */
-	public SetShooterVelocity(boolean isAuto) {
+	public CheesySetShooterVelocity(boolean isAuto) {
 		this(ShooterVelocity.kDefault, isAuto);
 	}
 
 	/**
 	 * @param velocity the velocity setpoint the talon fx will try to achieve.
 	 */
-	public SetShooterVelocity(ShooterVelocity velocity) {
+	public CheesySetShooterVelocity(ShooterVelocity velocity) {
 		this(velocity, false);
 	}
 
@@ -52,14 +56,14 @@ public class SetShooterVelocity extends CommandBase {
 	 * @param velocity the velocity setpoint the talon fx will try to achieve.
 	 * @param isAuto   whether the command should stop after shooting 3 cells
 	 */
-	public SetShooterVelocity(ShooterVelocity velocity, boolean isAuto) {
+	public CheesySetShooterVelocity(ShooterVelocity velocity, boolean isAuto) {
 		this(velocity.getVelocity(), isAuto);
 	}
 
 	/**
 	 * @param velocitySetpoint The setpoint used for calculation the PID velocity error in RPM.
 	 */
-	public SetShooterVelocity(double velocitySetpoint) {
+	public CheesySetShooterVelocity(double velocitySetpoint) {
 		this(velocitySetpoint, false);
 	}
 
@@ -67,15 +71,15 @@ public class SetShooterVelocity extends CommandBase {
 	 * @param velocitySetpoint The setpoint used for calculation the PID velocity error in RPM.
 	 * @param isAuto           Whether the command is run in autonomous.
 	 */
-	public SetShooterVelocity(double velocitySetpoint, boolean isAuto) {
+	public CheesySetShooterVelocity(double velocitySetpoint, boolean isAuto) {
 		this(() -> velocitySetpoint, isAuto);
 	}
 
-	public SetShooterVelocity(DoubleSupplier velocitySetpointSupplier) {
+	public CheesySetShooterVelocity(DoubleSupplier velocitySetpointSupplier) {
 		this(velocitySetpointSupplier, false);
 	}
 
-	public SetShooterVelocity(DoubleSupplier velocitySetpointSupplier, boolean isAuto) {
+	public CheesySetShooterVelocity(DoubleSupplier velocitySetpointSupplier, boolean isAuto) {
 		addRequirements(shooter);
 		velocitySetpoint = velocitySetpointSupplier;
 		this.isAuto = isAuto;
@@ -84,14 +88,47 @@ public class SetShooterVelocity extends CommandBase {
 	@Override
 	public void initialize() {
 		setpoint = velocitySetpoint.getAsDouble();
+		kfSamplesAmount = 0;
+		leftKfSamplesSum = 0;
+		rightKfSamplesSum = 0;
 		cellsShot = 0;
 		firstTimeOutsideZone = 0;
 		isInZone = true;
+		shooter.setVelocity(setpoint);
 	}
 
 	@Override
 	public void execute() {
+		if (kfSamplesAmount < MINIMUM_KF_SAMPLES)
+			// reach target velocity in closed loop and calculate kF
+			spinUpExecute();
+		else
+			// switch to open loop with calculated kF
+			holdExecute();
+	}
+
+	private void spinUpExecute() {
 		shooter.setVelocity(setpoint);
+		// very important - we use left shooter settings tolerance
+		boolean onTarget = Math.abs(shooter.getAverageSpeed() - setpoint) <
+				robotConstants.controlConstants.leftShooterSettings.getTolerance();
+		if (onTarget)
+			updateKf();
+		else {
+			// reset kF since we are to far
+			kfSamplesAmount = 0;
+			leftKfSamplesSum = 0;
+			rightKfSamplesSum = 0;
+		}
+	}
+
+	private void holdExecute() {
+		double leftFeedforward = setpoint * leftKfSamplesSum / kfSamplesAmount;
+		double rightFeedforward = setpoint * rightKfSamplesSum / kfSamplesAmount;
+		shooter.setPower(leftFeedforward, rightFeedforward);
+		// The shooter might heat up after extended use so we need to make sure to update kF if the shooter too much fast.
+		if (shooter.getAverageSpeed() > setpoint)
+			updateKf();
 		// If in auto, check how many cells were shot.
 		if (isAuto) {
 			boolean isCellBeingShot = shooter.isSwitchPressed();
@@ -100,7 +137,6 @@ public class SetShooterVelocity extends CommandBase {
 			countShotCells(isCellBeingShot);
 		}
 	}
-
 
 	private void countShotCells(boolean isCellBeingShot) {
 		if (!isInZone && isCellBeingShot &&
@@ -113,6 +149,12 @@ public class SetShooterVelocity extends CommandBase {
 			isInZone = false;
 			firstTimeOutsideZone = Timer.getFPGATimestamp();
 		}
+	}
+
+	private void updateKf() {
+		leftKfSamplesSum += Shooter.estimateKf(shooter.getLeftSpeed(), shooter.getLeftVoltage());
+		rightKfSamplesSum += Shooter.estimateKf(shooter.getRightSpeed(), shooter.getRightVoltage());
+		kfSamplesAmount++;
 	}
 
 	@Override
--- src/main/java/frc/robot/utils/DashboardDataContainer.java
+++ src/main/java/frc/robot/utils/DashboardDataContainer.java
@@ -3,7 +3,7 @@ package frc.robot.utils;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import frc.robot.Robot;
-import frc.robot.subsystems.shooter.SetShooterVelocity;
+import frc.robot.subsystems.shooter.CheesySetShooterVelocity;
 import frc.robot.subsystems.shooter.ShooterVelocity;
 
 import static frc.robot.Robot.shooter;
@@ -22,7 +22,7 @@ public class DashboardDataContainer {
 		dashboardController.addNumber("Shooter/LeftShooterVelocity", Robot.shooter::getLeftSpeed);
 		dashboardController.addNumber("Shooter/RightShooterVelocity", Robot.shooter::getRightSpeed);
 		SmartDashboard.putNumber("Shooter/Shooting Velocity Setpoint", ShooterVelocity.kDefault.getVelocity());
-		SmartDashboard.putData("Shooter/Shoot ball", new SetShooterVelocity(() ->
+		SmartDashboard.putData("Shooter/Shoot ball", new CheesySetShooterVelocity(() ->
 				SmartDashboard.getNumber("Shooter/Shooting Velocity Setpoint", 0)));
 		SmartDashboard.putData("Shooter/Enable Tuning",
 				new StartEndCommand(shooter::enableTuning, shooter::disableTuning));
