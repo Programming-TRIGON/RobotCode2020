c263740106f7c728704bc6cf42c18b919a450260
EliyaFishman <45822259+EliyaFishman@users.noreply.github.com>
2020-01-21 14:43:58 +0200

    remove SetIntakeSpeed

--- src/main/java/frc/robot/utils/DashboardDataContainer.java
+++ src/main/java/frc/robot/utils/DashboardDataContainer.java
@@ -1,8 +1,5 @@
 package frc.robot.utils;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import frc.robot.subsystems.intake.SetIntakeSpeed;
-
 /**
  * DashboardDataContainer contains all the data to be viewed or put in the dashboard.
  */
@@ -10,11 +7,7 @@ public class DashboardDataContainer {
     private DashboardController dashboardController;
 
     public DashboardDataContainer() {
-        // Intake data
-        SmartDashboard.putNumber("Intake/Intake Power", 0);
         dashboardController = new DashboardController();
-        SmartDashboard.putData("Intake/Spin Intake",
-                new SetIntakeSpeed(() -> SmartDashboard.getNumber("Intake/Intake Power", 0)));
         // dashboardController.addBoolean(name, booleanSupplier);
         // dashboardController.addNumber(name, numberSupplier);
         // dashboardController.addString(name, stringSupplier);
